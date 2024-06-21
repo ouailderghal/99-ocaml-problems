920c4320c67ef5ef7ecbebb077942d9ec410d1f8
Ouail Derghal <ouailderghal@protonmail.com>
2024-06-21 11:41:46 +0200

    Add todos to 02.ml

--- 02.ml
+++ 02.ml
@@ -4,3 +4,6 @@ let rec last_two = function
   | [] | [_] -> None
   | [x; y] -> Some (x, y)
   | _ :: rest -> last_two rest;;
+
+(* TODO: write an equivalent function with typed parameters *)
+(* TODO: write an equivalent function that operates only on a list of integres *)
