0da0550afd1fce06d417f348198c67ff17faa02b
Ouail Derghal <ouailderghal@protonmail.com>
2024-06-21 10:23:31 +0200

    Update problem 01.ml

--- 01.ml
+++ 01.ml
@@ -1,5 +1,11 @@
-let rec last (xs: 'a list): 'a option = 
-  match xs with
-  | [] -> none
-  | [x] -> some x
-  | _ :: xs -> last(xs)
+(* 
+   Write a function last : 'a list -> 'a option that returns the last
+   element of a list
+*)
+
+let rec last (lst : 'a list) : 'a option  =
+  match lst with
+  | [] -> None
+  | [x] -> Some x
+  | _ :: rest -> last(rest);;
+
