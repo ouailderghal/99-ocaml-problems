dc15e451612eaa44f3161c5fd17ea7fd110dbf7e
Ouail Derghal <ouailderghal@protonmail.com>
2024-06-22 07:25:11 +0200

    Add 04.ml

---
+++ 04.ml
@@ -0,0 +1,27 @@
+(* Length of a list *)
+let length lst =
+  let rec aux n = function
+    | [] -> n
+    | _ :: tail -> aux (n + 1) tail
+  in
+    aux 0 lst;;
+
+
+(* Length of a list with typed arguments *)
+let length_typed (lst: 'a list) : int =
+  let rec aux (n: int) (lst: 'a list) : int =
+    match lst with
+    | [] -> n
+    | _ :: tail -> aux (n + 1) tail
+  in
+    aux 0 lst;;
+
+
+(* Length of a list of integers *)
+let length_int (lst: int list) : int =
+  let rec aux (n: int) (lst: int list) : int =
+    match lst with
+    | [] ->  n
+    | _ :: tail -> aux (n + 1) tail
+  in
+    aux 0 lst;;
